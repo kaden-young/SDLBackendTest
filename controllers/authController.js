e6c2bb47cb356fde53fba3a13feca56baf2834ed
kaden-young <youngkj@msoe.edu>
2023-01-22 16:04:02 -0600

    redo

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
-    const cookies = req.cookies;
-    console.log(`cookie available at login: ${JSON.stringify(cookies)}`);
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
 
@@ -25,47 +23,19 @@ const handleLogin = async (req, res) => {
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '10s' }
         );
-        const newRefreshToken = jwt.sign(
+        const refreshToken = jwt.sign(
             { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
-
-        // Changed to let keyword
-        let newRefreshTokenArray =
-            !cookies?.jwt
-                ? foundUser.refreshToken
-                : foundUser.refreshToken.filter(rt => rt !== cookies.jwt);
-
-        if (cookies?.jwt) {
-
-            /* 
-            Scenario added here: 
-                1) User logs in but never uses RT and does not logout 
-                2) RT is stolen
-                3) If 1 & 2, reuse detection is needed to clear all RTs when user logs in
-            */
-            const refreshToken = cookies.jwt;
-            const foundToken = await User.findOne({ refreshToken }).exec();
-
-            // Detected refresh token reuse!
-            if (!foundToken) {
-                console.log('attempted refresh token reuse at login!')
-                // clear out ALL previous refresh tokens
-                newRefreshTokenArray = [];
-            }
-
-            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-        }
-
         // Saving refreshToken with current user
-        foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+        foundUser.refreshToken = refreshToken;
         const result = await foundUser.save();
         console.log(result);
         console.log(roles);
 
         // Creates Secure Cookie with refresh token
-        res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
         // Send authorization roles and access token to user
         res.json({ roles, accessToken });
